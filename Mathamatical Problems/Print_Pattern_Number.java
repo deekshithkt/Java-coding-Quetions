c291cbd3b91e52930af8b8e9400ecf7bbdaa6fea
deekshithkt <deelakshak@gmail.com>
2025-02-21 16:51:26 +0530

    math 2nd commt

--- Mathamatical Problems/Print_Pattern_Number.java
+++ Mathamatical Problems/Print_Pattern_Number.java
@@ -1,6 +1,32 @@
+// to print the patter like below 
+// For n= 3,
+// pattern:         3 3 3 2 2 2 1 1 1
+//                     3 3 2 2 1 1 
+//                     3 2 1
 
+// For n= 2,
+// pattern:  2 2 1 1
+//             2 1
+
+
+
+// Input: 2
+// Output: [2, 2, 1, 1, -1, 2, 1, -1]
+// Input: 3
+// Output: [3, 3, 3, 2, 2, 2, 1, 1, 1, -1, 3, 3, 2, 2, 1, 1, -1, 3, 2, 1, -1]
+// Input: 1
+// Output: [1, -1]
+import java.util.Scanner;
 class Print_Pattern_Number{
     public static void main(String args[]){
-        System.out.println("hfdfhd");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to print :");
+        int n=sc.nextInt();
+
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(i +" ");
+            }
+        }
     }
 }
