5b0d58ec4cd66df758883d08212f5a32e0223403
deekshithkt <deelakshak@gmail.com>
2025-02-24 18:25:16 +0530

    main method

--- Tree/Main.java
+++ Tree/Main.java
@@ -3,8 +3,11 @@ package Tree;
 public class Main  {
     public static void main(String[] args){
        Display display=new Display();
+       Search search=new Search();
        display.insert(1);
        display.insert(2);
        display.Display_Tree();
+       search.search(1);
+
     }
 }
