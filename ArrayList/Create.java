2b4f03463600a4091f4c200c674380c5ad6fa615
deekshithkt <deelakshak@gmail.com>
2025-02-24 18:35:40 +0530

    my first arraylist commit

---
+++ ArrayList/Create.java
@@ -0,0 +1,13 @@
+package ArrayList;
+import java.util.*;
+public class Create {
+    
+    public static void main(String[] args){
+        ArrayList<String> list=new ArrayList<String>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        
+        System.out.println(list.get(2));
+    }
+}
